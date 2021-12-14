d6479bddbe6769fd48d99a677164e1b238b096da
lunaharre <93848989+lunaharre@users.noreply.github.com>
2021-12-13 21:36:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# Luna Harre
+# WorldFacts - A quiz on how knowledgable one is on world facts
+# References: Gapminder.org, w3schools.com, Alex Patterson
+import Functions
+import textDudes
+print("Welcome to the World Fact Game!!")
+Name = input("Enter your name: ")
+Score = 0
+
+Functions.quiz1(textDudes.Question1, textDudes.Answer1, textDudes.Correct1, Score)
+Functions.quiz1(textDudes.Question2, textDudes.Answer2, textDudes.Correct2, Score)
+Functions.quiz1(textDudes.Question3, textDudes.Answer3, textDudes.Correct3, Score)
+Functions.quiz1(textDudes.Question4, textDudes.Answer4, textDudes.Correct4, Score)
+Functions.quiz1(textDudes.Question5, textDudes.Answer5, textDudes.Correct5, Score)
+Functions.quiz1(textDudes.Question6, textDudes.Answer6, textDudes.Correct6, Score)
+Functions.quiz1(textDudes.Question7, textDudes.Answer7, textDudes.Correct7, Score)
+Functions.quiz1(textDudes.Question8, textDudes.Answer8, textDudes.Correct8, Score)
+Functions.quiz1(textDudes.Question9, textDudes.Answer9, textDudes.Correct9, Score)
+
+
+
+
---
+++ textDudes.py
@@ -0,0 +1,32 @@
+# Luna Harre
+# WorldFacts - A quiz on how knowledgable one is on world facts
+# References: Gapminder.org, w3schools.com, Alex Patterson
+Question1 = "Question one: What percent of energy used in the world comes from " \
+           "natural gas, coal and oil?"
+Answer1 = "Enter A for 42, Enter B for 62, Enter C for 82: "
+Correct1 = "C"
+Question2 = "Question two: What percentage of people in the world have access to clean water?"
+Answer2 = "Enter A for 30, Enter B for 50, Enter C for 70: "
+Correct2 = "C"
+Question3 = "Question three: How much of the worlds plastic waste ends up in the oceans?"
+Answer3 = "Enter A for less than 6 percent, Enter B for around 32 percent, Enter C for more than 65 percent: "
+Correct3 = "A"
+Question4 = "Question four: How many countries in the world have female leaders in executive power?"
+Answer4 = "Enter A for 28 countries, Enter B for 14 countries, Enter C for 50 countries: "
+Correct4 = "B"
+Question5 = "Question five: There are roughly eight billion people in the world today. " \
+            "What continent has the largest percentage of people? "
+Answer5 = "Enter A for Europe, Enter B for North America, Enter C for Asia"
+Correct5 = "C"
+Question6 = "Question six: What share of the world population are international refugees?"
+Answer6 = "Enter A for around 0.4%, Enter B for around 4.4%, Enter C for around 14.4%"
+Correct6 = "A"
+Question7 = "Question seven: How much of the excess heat from global warming is captured in the oceans?"
+Answer7 = "Enter 10 for 10%, Enter 50 for 50%, Enter 90 for 90%"
+Correct7 = "90"
+Question8 = "Question eight: What happened to the global suicide rate in the last 20 years?"
+Answer8 = "Enter A for decreased about 25%, Enter B for stayed the same, Enter C for increased about 25%"
+Correct8 = "A"
+Question9 = "True or False?: The moon is wider than Australia."
+Answer9 = "Enter T or F"
+Correct9 = "F"
